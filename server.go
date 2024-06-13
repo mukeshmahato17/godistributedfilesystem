304646483af9b6e0b0c8c27c1344b5a802e3de74
mukeshmahato17 <mukeshmahato1089@gmail.com>
2024-06-13 19:10:09 +0545

    implemented OnPeer func

--- server.go
+++ server.go
@@ -19,10 +19,12 @@ type FileServerOpts struct {
 
 type FileServer struct {
 	FileServerOpts
+
 	peerLock sync.Mutex
 	peers    map[string]p2p.Peer
-	store    *Store
-	quitch   chan struct{}
+
+	store  *Store
+	quitch chan struct{}
 }
 
 func NewFileServer(opts FileServerOpts) *FileServer {
@@ -40,6 +42,17 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 
 func (s *FileServer) Stop() {
 	close(s.quitch)
+}
+
+func (s *FileServer) OnPeer(p p2p.Peer) error {
+	s.peerLock.Lock()
+	defer s.peerLock.Unlock()
+
+	s.peers[p.RemoteAddr().String()] = p
+
+	log.Printf("connected with remote %s", p.RemoteAddr())
+
+	return nil
 }
 
 func (s *FileServer) Store(key string, r io.Reader) error {
