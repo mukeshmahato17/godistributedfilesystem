2b7ea7a4385f8face404efb4f752982c58f30566
mukeshmahato17 <mukeshmahato1089@gmail.com>
2024-06-14 21:29:06 +0545

    tcp_transport.go

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -24,6 +24,17 @@ func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	}
 }
 
+func (p *TCPPeer) Send(b []byte) error {
+	_, err := p.conn.Write(b)
+	return err
+}
+
+// RemoteAddr implements the Peer interface and will return the
+// remote address of its underlying connection.
+func (p *TCPPeer) RemoteAddr() net.Addr {
+	return p.conn.RemoteAddr()
+}
+
 // Close implements the peer interfac.
 func (p *TCPPeer) Close() error {
 	return p.conn.Close()
@@ -96,7 +107,6 @@ func (t *TCPTransport) startAccepttLoop() {
 			fmt.Printf("tcp error %s\n", err)
 		}
 
-		fmt.Printf("new incomming connection +%v\n", conn)
 		go t.handleConn(conn, false)
 	}
 }
