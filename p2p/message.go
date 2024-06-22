ae70eaeeeebdb6f002cb0b15bb3b07c0f3108bbf
mukeshmahato17 <mukeshmahato1089@gmail.com>
2024-06-22 15:36:06 +0545

    message

--- p2p/message.go
+++ p2p/message.go
@@ -1,10 +1,8 @@
 package p2p
 
-import "net"
-
 // Message holds any arbitrary that is being sent over the
 // each transport between two nodes in the network.
 type RPC struct {
-	From    net.Addr
-	payload []byte
+	From    string
+	Payload []byte
 }
