9a2cd89ead1958bd5f895c0e1d2faba7154a10af
mukeshmahato17 <mukeshmahato1089@gmail.com>
2024-06-23 21:55:42 +0545

    sync

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 )
 
 // TCPPeer represents the remote node over a TCP established connection.
@@ -16,12 +17,15 @@ type TCPPeer struct {
 	// if we dial and retrieve a conn => outbound == true
 	// if we accept and retrieve a conn => outbound == false
 	outbound bool
+
+	Wg *sync.WaitGroup
 }
 
 func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	return &TCPPeer{
 		Conn:     conn,
 		outbound: outbound,
+		Wg:       &sync.WaitGroup{},
 	}
 }
 
@@ -130,10 +134,12 @@ func (t *TCPTransport) handleConn(conn net.Conn, outbound bool) {
 			return
 		}
 
-		rpc.From = conn.RemoteAddr()
+		rpc.From = conn.RemoteAddr().String()
+		peer.Wg.Add(1)
+		fmt.Println("waiting till stream is done")
 		t.rpcch <- rpc
-
-		fmt.Printf("message: %+v", rpc)
+		peer.Wg.Wait()
+		fmt.Println("stream done continueing tthe normal read loop")
 
 	}
 }
